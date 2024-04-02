868d34c7d41586ae5cd8dd2fb5f029d04dbb615b
Kboere <kevin.boere@hva.nl>
2024-04-02 16:18:58 +0200

    fetch api and search function

--- server.js
+++ server.js
@@ -10,18 +10,52 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+// Define fetch function
+async function fetchMovies(apiToken) {
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiToken}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=2010-01-01`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.results;
+}
 
 // Define routes
 app.get("/", async function (req, res) {
-  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_TOKEN}&language=en-US&page=1`;
+  try {
+    const movies = await fetchMovies(process.env.API_TOKEN);
+    res.render('pages/index', { movies });
+  } catch (error) {
+    console.error('Fetching movies failed:', error);
+    res.status(500).send('Failed to fetch movies');
+  }
+});
+
+// Search route
+app.get('/search', async (req, res) => {
+  let movies = [];
+  let title = 'Search Movies';
+  let { search } = req.query;
 
   try {
+    const apiToken = process.env.API_TOKEN;
+    if (search) {
+      // Corrected URL to use the search endpoint instead of the discover endpoint
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiToken}&language=en-US&query=${encodeURIComponent(search)}&page=1&include_adult=false&sort_by=popularity.desc&primary_release_date.gte=2010-01-01`;
       const response = await fetch(url);
       const data = await response.json();
-      res.render('pages/index', { movies: data.results, title: 'Popular Movies' });
+      movies = data.results;
+      title = `Search Results for "${search}"`;
+      console.log(`Search query: ${search}`);
+    } else {
+      // If no search query, show default movies
+      console.log('No search query, fetching default movies');
+      movies = await fetchMovies(apiToken);
+    }
+
+    // Render the search page with either search results or default movies
+    res.render('pages/search', { movies, title, searchQuery: search });
   } catch (error) {
-      console.error('Fetching movies failed:', error);
-      res.status(500).send('Failed to fetch movies');
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
