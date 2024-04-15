76b8b72d821c428a9bf2455bc2af46ac9a8c2160
Kevin Boere <kevin.boere@hva.nl>
2024-04-15 16:10:45 +0200

    fix single qoute

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const movies = document.querySelectorAll(".movie");
-  const modal = document.getElementById("myModal");
-  const modalTitle = document.getElementById("modal-title");
-  const modalOverview = document.getElementById("modal-overview");
-  const closeModal = document.querySelector(".close");
+document.addEventListener('DOMContentLoaded', function () {
+  const movies = document.querySelectorAll('.movie');
+  const modal = document.getElementById('myModal');
+  const modalTitle = document.getElementById('modal-title');
+  const modalOverview = document.getElementById('modal-overview');
+  const closeModal = document.querySelector('.close');
 
   movies.forEach(movie => {
-    movie.addEventListener("click", async function() {
-      const movieId = this.getAttribute("data-movie-id");
+    movie.addEventListener('click', async function () {
+      const movieId = this.getAttribute('data-movie-id');
 
       try {
         // Send a request to the server to fetch movie details
@@ -19,22 +19,22 @@ document.addEventListener("DOMContentLoaded", function() {
         modalOverview.textContent = movieDetails.overview;
 
         // Display the modal
-        modal.style.display = "block";
+        modal.style.display = 'block';
       } catch (error) {
-        console.error("Error fetching movie details:", error);
+        console.error('Error fetching movie details:', error);
       }
     });
   });
 
   // Close the modal when the close button is clicked
-  closeModal.addEventListener("click", function() {
-    modal.style.display = "none";
+  closeModal.addEventListener('click', function () {
+    modal.style.display = 'none';
   });
 
   // Close the modal when the overlay outside the modal is clicked
-  window.addEventListener("click", function(event) {
+  window.addEventListener('click', function (event) {
     if (event.target == modal) {
-      modal.style.display = "none";
+      modal.style.display = 'none';
     }
   });
 });
